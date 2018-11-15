87e7cdbd8606c7a2fe6bf0de5e80383e3029f456
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-11-15 18:37:00 +0200

    updated

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,20 +1,21 @@
 var models = require('../models');
-
+ // this function excute inside router
 module.exports = {
   messages: {
     get: function (req, res) {
+      // call the message.get from modul to apply it;
       models.messages.get(function(data){
-        res.send(JSON.stringify({results:data}));
-        
+        res.json({results:data});
       });
-      //console.log( models.messages.get());
-
-
     }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
-  },
-
-  users: {
+    post: function (req, res) {
+      var params = [req.body[text], req.body[roomname], req.body[username]];
+      // call the message.get from modul to apply it;
+      models.messages.post(params, function(data){
+        res.json('done');
+      }) // a function which handles posting a message to the database
+    },
+    users: {
     // Ditto as above
     get: function (req, res) {},
     post: function (req, res) {}
--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,14 @@
 var controller = require('./controllers');
 var router = require('express').Router();
-
+// work with router express
 //Connect controller methods to their corresponding routes
 router.get('/messages', function(req, res) {
   controller.messages.get(req, res);
 })
 
-router.post('/messages', controller.messages.post);
+router.post('/messages', function(req, res) {
+  controller.messages.post(req, res);
+});
 
 //router.options('/messages', controller.messages.post);
 router.get('/users', controller.users.get);
